edf14028f6444cec55261a452bc052ea1f9495a4
BarikQ <yar.barik99@gmail.com>
2019-06-30 19:57:43 +0300

    bitch

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -15,10 +15,9 @@ function onSignIn(googleUser) {
 }
 
 function signOut() {
-  let auth2 = gapi.auth2.getAuthInstaance();
+  let auth2 = gapi.auth2.getAuthInstance();
   auth2.signOut().then(function() {
     alert('sign out');
-
     $('.g-signin2').css('display', 'block');
     $('.userData').css('display', 'none');
   });
