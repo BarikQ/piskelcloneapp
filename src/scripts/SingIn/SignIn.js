bda56754f53d22977775528984e1048f9507adb3
BarikQ <yar.barik99@gmail.com>
2019-06-30 19:37:47 +0300

    hsdhs

--- src/scripts/SingIn/SignIn.js
+++ src/scripts/SingIn/SignIn.js
@@ -1,6 +1,9 @@
-export default class SignIn {
-  render() {
-    console.log(1);
-    const UserId = `1087138147820-h0vnaq3268gj241ctokv99ifr3jj25a3.apps.googleusercontent.com`;
-  }
+const UserId = `1087138147820-h0vnaq3268gj241ctokv99ifr3jj25a3.apps.googleusercontent.com`;
+
+function onSignIn(googleUser) {
+  let profile = googleUser.getBasicProfile();
+  $('.g-sign-2').css('display', 'none');
+  $('.data').css('display', 'block');
+  $('#userPic').attr('src', profile.getImageUrl());
+  $('#email').text(profile.getEmail());
 }
