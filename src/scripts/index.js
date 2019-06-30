20cbef7c9da6b43bc235235a3b1f98383a4d3b3e
BarikQ <yar.barik99@gmail.com>
2019-06-30 20:05:07 +0300

    asgaso

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,10 +2,7 @@ import App from './controller/App';
 
 const app = new App();
 
-app.start();
-
 const UserId = `1087138147820-h0vnaq3268gj241ctokv99ifr3jj25a3.apps.googleusercontent.com`;
-
 function onSignIn(googleUser) {
   let profile = googleUser.getBasicProfile();
   $('.g-signin2').css('display', 'none');
@@ -14,11 +11,13 @@ function onSignIn(googleUser) {
   $('#email').text(profile.getEmail());
 }
 
-// function signOut() {
-//   let auth2 = gapi.auth2.getAuthInstance();
-//   auth2.signOut().then(function() {
-//     alert('sign out');
-//     $('.g-signin2').css('display', 'block');
-//     $('.userData').css('display', 'none');
-//   });
-// }
+function signOut() {
+  let auth2 = gapi.auth2.getAuthInstance();
+  auth2.signOut().then(function() {
+    alert('sign out');
+    $('.g-signin2').css('display', 'block');
+    $('.userData').css('display', 'none');
+  });
+}
+
+app.start();
