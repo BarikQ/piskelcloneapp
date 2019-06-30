fd52ee2b35c15677da5bcd5cca1ade6f2e4ba553
BarikQ <yar.barik99@gmail.com>
2019-06-30 19:45:36 +0300

    aep[k

--- src/scripts/SingIn/SignIn.js
+++ src/scripts/SingIn/SignIn.js
@@ -2,7 +2,7 @@ const UserId = `1087138147820-h0vnaq3268gj241ctokv99ifr3jj25a3.apps.googleuserco
 
 function onSignIn(googleUser) {
   let profile = googleUser.getBasicProfile();
-  $('.g-sign-2').css('display', 'none');
+  $('.g-signin2').css('display', 'none');
   $('.userData').css('display', 'block');
   $('#userPic').attr('src', profile.getImageUrl());
   $('#email').text(profile.getEmail());
