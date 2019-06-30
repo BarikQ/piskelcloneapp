02cb8ade999f3a77ad08d0c1bae6894526f42cdf
BarikQ <yar.barik99@gmail.com>
2019-06-30 21:30:19 +0300

    oih;gdf[paw

--- src/scripts/SingIn/SignIn.js
+++ src/scripts/SingIn/SignIn.js
@@ -1,17 +1,50 @@
-const UserId = `1087138147820-h0vnaq3268gj241ctokv99ifr3jj25a3.apps.googleusercontent.com`;
-function onSignIn(googleUser) {
-  let profile = googleUser.getBasicProfile();
-  $('.g-signin2').css('display', 'none');
-  $('.userData').css('display', 'block');
-  $('#userPic').attr('src', profile.getImageUrl());
-  $('#email').text(profile.getEmail());
+/* eslint-disable */
+
+function renderButton() {
+  gapi.signin2.render('gSignIn', {
+    scope: 'profile email',
+    width: 50,
+    height: 50,
+    longtitle: true,
+    theme: 'dark',
+    onsuccess: onSuccess,
+    onfailure: onFailure
+  });
+}
+
+function onSuccess(googleUser) {
+  gapi.client.load('oauth2', 'v2', function() {
+    var request = gapi.client.oauth2.userinfo.get({
+      userId: 'me'
+    });
+    request.execute(function(resp) {
+      let profileHTML =
+        '<h3>Welcome ' +
+        resp.given_name +
+        '! </h3>' +
+        '<img class="userPic" src="' +
+        resp.picture +
+        '"/>' +
+        '<a href="javascript:void(0);" onclick="signOut();">Sign out</a>';
+      document.getElementsByClassName('userContent')[0].innerHTML = profileHTML;
+
+      document.getElementById('gSignIn').style.display = 'none';
+      document.getElementsByClassName('userContent')[0].style.display = 'grid';
+    });
+  });
+}
+
+function onFailure(error) {
+  alert(error);
 }
 
 function signOut() {
-  let auth2 = gapi.auth2.getAuthInstance();
+  var auth2 = gapi.auth2.getAuthInstance();
   auth2.signOut().then(function() {
-    alert('sign out');
-    $('.g-signin2').css('display', 'block');
-    $('.userData').css('display', 'none');
+    document.getElementsByClassName('userContent')[0].innerHTML = '';
+    document.getElementsByClassName('userContent')[0].style.display = 'none';
+    document.getElementById('gSignIn').style.display = 'grid';
   });
+
+  auth2.disconnect();
 }
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,22 +2,4 @@ import App from './controller/App';
 
 const app = new App();
 
-const UserId = `1087138147820-h0vnaq3268gj241ctokv99ifr3jj25a3.apps.googleusercontent.com`;
-function onSignIn(googleUser) {
-  let profile = googleUser.getBasicProfile();
-  $('.g-signin2').css('display', 'none');
-  $('.userData').css('display', 'block');
-  $('#userPic').attr('src', profile.getImageUrl());
-  $('#email').text(profile.getEmail());
-}
-
-function signOut() {
-  let auth2 = gapi.auth2.getAuthInstance();
-  auth2.signOut().then(function() {
-    alert('sign out');
-    $('.g-signin2').css('display', 'block');
-    $('.userData').css('display', 'none');
-  });
-}
-
 app.start();
