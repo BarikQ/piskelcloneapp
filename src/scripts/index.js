01300d4277de4f7995c073040708151ccb9578e3
BarikQ <yar.barik99@gmail.com>
2019-06-30 19:50:57 +0300

    gasgk'as['pk

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,15 @@
 import App from './controller/App';
 
+const UserId = `1087138147820-h0vnaq3268gj241ctokv99ifr3jj25a3.apps.googleusercontent.com`;
+
+function onSignIn(googleUser) {
+  let profile = googleUser.getBasicProfile();
+  $('.g-signin2').css('display', 'none');
+  $('.userData').css('display', 'block');
+  $('#userPic').attr('src', profile.getImageUrl());
+  $('#email').text(profile.getEmail());
+}
+
 const app = new App();
 
 app.start();
